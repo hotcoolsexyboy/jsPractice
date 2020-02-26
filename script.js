2c210477b3f04489b553f49008f0ce0ee1c91d4d
Vladislav Gavrish <vdgavrish@gmail.com>
2020-02-26 17:50:04 +0300

    the third task

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
-"use strict";
+(function () {
+  'use strict';
+}());
 
-let money = +prompt('Ваш бюджет на месяц?', ''),
-    time = prompt('Введите дату в формате YYYY-MM-DD', '');
+let money, time;
+
+function start() {
+  money = +prompt('Ваш бюджет на месяц?', '');
+  time = prompt('Введите дату в формате YYYY-MM-DD', '');
+
+  while(isNaN(money) || money == null || money == '') {
+    money = +prompt(
+      'Некорректное значение. Введите ваш бюджет на месяц в числовом формате'
+      );
+  }
+}
+
+start();                //function asks money and date
 
 let appData = {
   monthlyBudget: money,
@@ -9,65 +23,63 @@ let appData = {
   expenses: {},
   optionalExpenses: {},
   income : [],
-  savings: false
+  savings: true
 };
 
-for(let i = 0; i < 2; i++){
-  let needs = prompt('Введите обязательную статью расходов в этом месяце', ''),
-      costs = prompt('Во сколько обойдется?', '');
-
-  if((typeof(needs)) === 'string' && needs.length < 10 &&
-      costs != null && costs != '' &&
-      needs != null && needs != '') {
-      console.log('Success!');
-      appData.expenses[needs] = costs;
-  } else {
-      console.log('Error!');
-      i--;
+function chooseExpenses() {
+  for(let i = 0; i < 2; i++){
+    let needs = prompt('Введите обязательную статью расходов в этом месяце'),
+        costs = prompt('Во сколько обойдется?', '');
+  
+    if((typeof(needs)) === 'string' && needs.length < 10 &&
+        costs != null && costs != '' &&
+        needs != null && needs != '') {
+        console.log('Success!');
+        appData.expenses[needs] = costs;
+    } else {
+        console.log('Error!');
+        i--;
+    }
   }
 }
 
-appData.dailyBudget = appData.monthlyBudget / 30;
-alert(`Your daily budget is $${appData.dailyBudget}`);
-
-if(appData.dailyBudget < 34) {
-  alert('You have pretty small income.');
-} else if (appData.dailyBudget >= 34 && appData.dailyBudget < 100) {
-  alert('You have medium income');
-} else if (appData.dailyBudget > 100) {
-  alert('You have high income');
-} else {
-  alert('Cannot calculate. Error!');
+function detectDayBudget() {
+  appData.dailyBudget = (appData.monthlyBudget / 30).toFixed(2);
+  alert(`Your daily budget is $${appData.dailyBudget}`);
 }
 
-// let i = 0;
-// do {
-//   let needs = prompt('Введите обязательную статью расходов в этом месяце', ''),
-//       costs = prompt('Во сколько обойдется?', '');  
+function detectLevel() {
+  if(appData.dailyBudget < 34) {
+    alert('You have pretty small income.');
+  } else if (appData.dailyBudget >= 34 && appData.dailyBudget < 100) {
+    alert('You have medium income');
+  } else if (appData.dailyBudget > 100) {
+    alert('You have high income');
+  } else {
+    alert('Cannot calculate. Error!');
+  }
+}
 
-//   if((typeof(needs)) === 'string' && needs.length < 10 &&
-//   costs != null && costs != '' &&
-//   needs != null && needs != '') {
-//     console.log('Success!');
-//     appData.expenses[needs] = costs;
-//     i++;
-//   } else {
-//       console.log('Error!');
-//   }  
-// } while(i < 2);
+function checkSavings() {
+  if(appData.savings) {
+    let save = +prompt('Какова сумма накоплений?', ''),
+        percent = +prompt('Под какой процент?', '');
 
-// let i = 0;
-// while(i < 2) {
-//   let needs = prompt('Введите обязательную статью расходов в этом месяце', ''),
-//       costs = prompt('Во сколько обойдется?', '');  
-  
-//   if((typeof(needs)) === 'string' && needs.length < 10 &&
-//       costs != null && costs != '' &&
-//       needs != null && needs != '') {
-//         console.log('Success!');
-//         appData.expenses[needs] = costs;
-//         i++;
-//   } else {
-//       console.log('Error!');
-//   }  
-// }
+    appData.monthIncome = save / 100 / 12 * percent;
+    alert("Доход в месяц с вашего депозита: " + appData.monthIncome);
+  }
+}
+
+function chooseOptExpenses() {
+  let optExpenses;
+  for(let i = 0; i < 3; i++) {
+    optExpenses = prompt('Статья необязательных расходов?');
+    appData.optionalExpenses[i] = optExpenses;
+  }
+}
+
+chooseExpenses();       //function asks about needs and costs
+detectDayBudget();      //function calculates money per day
+detectLevel();          //function says user's salary level
+checkSavings();         //function asks saves and percent
+chooseOptExpenses();    //function asks about optional expenses
