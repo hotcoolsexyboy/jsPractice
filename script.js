26f5e2bd7761e31549458527d125d840cb2b520b
Vladislav Gavrish <vdgavrish@gmail.com>
2020-03-07 12:31:41 +0300

    functions -> methods of appData

--- script.js
+++ script.js
@@ -23,63 +23,52 @@ let appData = {
   expenses: {},
   optionalExpenses: {},
   income : [],
-  savings: true
-};
-
-function chooseExpenses() {
-  for(let i = 0; i < 2; i++){
-    let needs = prompt('Введите обязательную статью расходов в этом месяце'),
-        costs = prompt('Во сколько обойдется?', '');
-  
-    if((typeof(needs)) === 'string' && needs.length < 10 &&
-        costs != null && costs != '' &&
-        needs != null && needs != '') {
-        console.log('Success!');
-        appData.expenses[needs] = costs;
+  savings: true,
+  chooseExpenses: function() {
+    for(let i = 0; i < 2; i++){
+      let needs = prompt('Введите обязательную статью расходов в этом месяце'),
+          costs = prompt('Во сколько обойдется?', '');
+    
+      if((typeof(needs)) === 'string' && needs.length < 10 &&
+          costs != null && costs != '' &&
+          needs != null && needs != '') {
+          console.log('Success!');
+          appData.expenses[needs] = costs;
+      } else {
+          console.log('Error!');
+          i--;
+      }
+    }
+  },
+  detectDayBudget: function() {
+    appData.dailyBudget = (appData.monthlyBudget / 30).toFixed(2);
+    alert(`Your daily budget is $${appData.dailyBudget}`);
+  },
+  detectLevel: function() {
+    if(appData.dailyBudget < 34) {
+      alert('You have pretty small income.');
+    } else if (appData.dailyBudget >= 34 && appData.dailyBudget < 100) {
+      alert('You have medium income');
+    } else if (appData.dailyBudget > 100) {
+      alert('You have high income');
     } else {
-        console.log('Error!');
-        i--;
+      alert('Cannot calculate. Error!');
+    }
+  },
+  checkSavings: function() {
+    if(appData.savings) {
+      let save = +prompt('Какова сумма накоплений?', ''),
+          percent = +prompt('Под какой процент?', '');
+  
+      appData.monthIncome = save / 100 / 12 * percent;
+      alert("Доход в месяц с вашего депозита: " + appData.monthIncome);
+    }
+  },
+  chooseOptExpenses: function() {
+    let optExpenses;
+    for(let i = 0; i < 3; i++) {
+      optExpenses = prompt('Статья необязательных расходов?');
+      appData.optionalExpenses[i] = optExpenses;
     }
   }
-}
-
-function detectDayBudget() {
-  appData.dailyBudget = (appData.monthlyBudget / 30).toFixed(2);
-  alert(`Your daily budget is $${appData.dailyBudget}`);
-}
-
-function detectLevel() {
-  if(appData.dailyBudget < 34) {
-    alert('You have pretty small income.');
-  } else if (appData.dailyBudget >= 34 && appData.dailyBudget < 100) {
-    alert('You have medium income');
-  } else if (appData.dailyBudget > 100) {
-    alert('You have high income');
-  } else {
-    alert('Cannot calculate. Error!');
-  }
-}
-
-function checkSavings() {
-  if(appData.savings) {
-    let save = +prompt('Какова сумма накоплений?', ''),
-        percent = +prompt('Под какой процент?', '');
-
-    appData.monthIncome = save / 100 / 12 * percent;
-    alert("Доход в месяц с вашего депозита: " + appData.monthIncome);
-  }
-}
-
-function chooseOptExpenses() {
-  let optExpenses;
-  for(let i = 0; i < 3; i++) {
-    optExpenses = prompt('Статья необязательных расходов?');
-    appData.optionalExpenses[i] = optExpenses;
-  }
-}
-
-chooseExpenses();       //function asks about needs and costs
-detectDayBudget();      //function calculates money per day
-detectLevel();          //function says user's salary level
-checkSavings();         //function asks saves and percent
-chooseOptExpenses();    //function asks about optional expenses
+};
