69636b253065e75358ca7a6c9b51fb6e319ff7d1
Vladislav Gavrish <vdgavrish@gmail.com>
2020-03-16 10:22:29 +0300

    add styles and get access to html through js

--- src/js/main.js
+++ src/js/main.js
@@ -15,7 +15,7 @@ function start() {
   }
 }
 
-start();                //function asks money and date
+//start();                //function asks money and date
 
 let appData = {
   monthlyBudget: money,
@@ -88,5 +88,30 @@ let appData = {
 };
 
 for (let key in appData) {
-  console.log("Наша программа включает в себя данные: " + key + " - " + appData[key]);
+  //console.log("Наша программа включает в себя данные: " + key + " - " + appData[key]);
 }
+
+let startCalc = document.getElementById('start'),
+    budgetValue = document.getElementsByClassName('budget-value')[0],
+    daybudgetValue = document.getElementsByClassName('daybudget-value')[0],
+    levelValue = document.getElementsByClassName('level-value')[0],
+    expensesValue = document.getElementsByClassName('expenses-value')[0],
+    optionalexpensesValue = document.getElementsByClassName('optionalexpenses-value')[0],
+    incomeValue = document.getElementsByClassName('income-value')[0],
+    monthsavingsValue = document.getElementsByClassName('monthsavings-value')[0],
+    yearsavingsValue = document.getElementsByClassName('yearsavings-value')[0],
+
+    expensesItemInput = document.getElementsByClassName('expenses-item'),
+    btnConfirm1 = document.getElementsByTagName('button')[0],
+    btnConfirm2 = document.getElementsByTagName('button')[1],
+    btnCalculate = document.getElementsByTagName('button')[2],
+    optionalexpensesItem = document.querySelectorAll('.optionalexpenses-item'),
+    chooseIncome = document.querySelector('.choose-income'),
+    checkbox = document.querySelector('#savings'),
+    chooseSum = document.querySelector('.choose-sum'),
+    choosePercent = document.querySelector('.choose-percent'),
+    year = document.querySelector('.year-value'),
+    month = document.querySelector('.month-value'),
+    day = document.querySelector('.day-value');
+
+console.log(choosePercent);
